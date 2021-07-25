e9b741c911feed9508298abdc99ed11ad74b5413
IGOR <kondrahov.2000@mail.ru>
2021-07-25 15:06:06 +0300

    Added App.vue with input form

---
+++ src/components/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div><input type="text" placeholder="name"></div>
+        <div><input type="number" placeholder="age"></div>
+        <div><input type="phone" placeholder="phone number"></div>
+        <div><input type="email" placeholder="email address"></div>
+        <button v-on:click="submit">submit</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data()
+    {
+        return {
+            line: "Hello, World!"
+        }
+    },
+    methods: {
+        submit: function()
+        {
+            this.line = this.line.split('').reverse().join('');
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
