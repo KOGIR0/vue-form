244324bdcbe523af1d38fe13bcaf9dd5e774968d
IGOR <kondrahov.2000@mail.ru>
2021-07-25 20:48:40 +0300

    Added styles

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,11 +1,19 @@
 <template>
-    <div>
-        <div><input type="text" placeholder="name" v-model="userName"></div>
-        <div><input type="number" placeholder="age" v-model="age"></div>
-        <div><input type="phone" placeholder="phone number" v-model="phone"></div>
-        <div><input type="email" placeholder="email address" v-model="email"></div>
-        <button v-on:click="submit">submit</button>
-    </div>
+    <form id="input-form" v-on:submit="submit">
+        <div class="input-field">
+            <input required type="text" placeholder="name" v-model="userName">
+        </div>
+        <div class="input-field">
+            <input required min="0" max="150" type="number" placeholder="age" v-model="age">
+        </div>
+        <div class="input-field">
+            <input required type="phone" placeholder="phone number" v-model="phone">
+        </div>
+        <div class="input-field">
+            <input required type="email" placeholder="email address" v-model="email">
+        </div>
+        <button type="submit" class="submit-button">submit</button>
+    </form>
 </template>
 
 <script>
@@ -20,8 +28,9 @@ export default {
         }
     },
     methods: {
-        submit: async function()
+        submit: async function(e)
         {
+            e.preventDefault();
             let data = {
                 "name": this.userName,
                 "age": this.age,
@@ -43,5 +52,39 @@ export default {
 </script>
 
 <style lang="less">
+#input-form
+{
+    background-color: orange;
+    box-sizing: border-box;
+    padding: 10px;
+    border: 2px solid black;
+    border-radius: 5px;
+    width: 250px;
+    text-align: center;
 
+    .input-field
+    {
+        input
+        {
+            color: black;
+            border: 1px solid darkorange;
+            padding: 3px;
+            margin: 5px;
+        }
+    }
+
+    .submit-button
+    {
+        width: 125px;
+        border: none;
+        color: black;
+        background-color: white;
+        border-radius: 2px;
+    }
+
+    .submit-button:hover
+    {
+        background-color: lightgrey;
+    }
+}
 </style>
