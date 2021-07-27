02947d123f0b3bd3bca252ac991c1d98abdef564
IGOR <kondrahov.2000@mail.ru>
2021-07-28 00:40:00 +0300

    Renamed App to Form

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
-import App from './components/App.vue';
+import Form from './components/Form.vue';
 
 new Vue({
     el: '#app',
-    render: h => h(App),
+    render: h => h(Form),
 });
