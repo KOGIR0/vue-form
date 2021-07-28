cc2409a2062a2c6354bbbc283bbb494ccfbc7291
IGOR <kondrahov.2000@mail.ru>
2021-07-28 11:49:33 +0300

    Added routing to the app

---
+++ src/components/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app">
+        <div id="navigation">
+            <router-link to="/users" class="route">Users</router-link>
+            <router-link to="/form" class="route">Form</router-link>
+        </div>
+        <router-view />
+    </div>
+</template>
+
+
+<style lang="less">
+#navigation
+{
+    display: flex;
+    flex-direction: row;
+    background-color: lightblue;
+    .route
+    {
+        padding: 5px;
+        text-decoration: none;
+        color: black;
+    }
+}
+</style>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+
 import Users from './components/Users.vue';
+import Form from './components/Form.vue';
+import App from './components/App.vue';
+
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false
+
+const routes = [
+    { path: "/users", component: Users },
+    { path: "/form", component: Form },
+    { path: "/", component: null }
+];
+
+const router = new VueRouter({
+    routes,
+    mode: "history"
+});
 
 new Vue({
+    router,
     el: '#app',
-    render: h => h(Users),
+    render: h => h(App)
 });
